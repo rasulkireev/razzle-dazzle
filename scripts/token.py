1c2bcdba52bf5e4d54301787b765b1edda815c03
Rasul Kireev <rasul.kireev@guycarp.com>
2021-08-30 17:27:26 -0400

    adding environment variables to python files

--- scripts/token.py
+++ scripts/token.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+import os
 
 from brownie import Token, accounts
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def main():
+    INFURA_PROJECT_ID = os.getenv("INFURA_PROJECT_ID")
     return Token.deploy("Test Token", "TST", 18, 1e21, {"from": accounts[0]})
