ba0bd7d3190c5da4d600ac33c87dece631b56cf6
Rasul Kireev <rasul.kireev@guycarp.com>
2021-08-31 10:50:58 -0400

    updated deploement script and brownie config, updated cicd pipelne to use potry

--- scripts/token.py
+++ scripts/token.py
@@ -2,11 +2,8 @@
 import os
 
 from brownie import Token, accounts
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 def main():
-    INFURA_PROJECT_ID = os.getenv("INFURA_PROJECT_ID")
-    return Token.deploy("Test Token", "TST", 18, 1e21, {"from": accounts[0]})
+    account = accounts.load("razzle-dazzle-wallet")
+    return Token.deploy("Razzle Dazzle", "RD", 18, 1e21, {"from": account})
